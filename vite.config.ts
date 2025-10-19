86000f28180bbc64c1a649c10a7f75b36a199446
Nguyen Le Phong - Senior Software Engineer Lead <phong.nguyen@ndsvn.vn>
2025-10-19 10:06:56 +0700

    feat(mfe): migration CRA to Vite

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/main.tsx',
+      name: 'ReactApp',
+      fileName: 'react-app',
+      formats: ['umd'],
+    },
+    rollupOptions: {
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
